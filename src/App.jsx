25e1cb65522809869766d71a337e7c7151b52751
JordanSchwartz97 <schwartzj1297@gmail.com>
2021-09-23 20:20:21 -0500

    Spotify clone now properly collects token from redirect login, beginning work on spotify player

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+
+const spotify = new SpotifyWebApi();
 
 function App(){
+    const [token, setToken] = useState();
+    //run code based on given condition
+    useEffect(() => {
+        const hash = getTokenFromUrl();
+        window.location.hash = '';
+        const _token = hash.access_token;
+
+        if (_token) {
+            setToken(_token)
+            spotify.setAccessToken(_token);
+            spotify.getMe().then(user => {
+                console.log(user);
+            })
+        }
+    }, []);
+    
+
     return(
-        <div className="app">
-            <Login />
+        <div className="app">{token ? 
+        <Player/>
+        : (<Login />)}
         </div>
     );
 }
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Login.css';
+import { loginUrl } from './spotify';
 
 function Login() {
     return (
         <div className="login">
             <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png" alt="spotify logo"/>
-            <a>LOGIN WITH SPOTIFY </a>
+            <a href={loginUrl}>LOGIN WITH SPOTIFY </a>
         </div>
     )
 }
